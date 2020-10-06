1c96220abad472d95a841eb35e429b35afdb5db2
stav rockah <stav@frontegg.com>
2020-10-07 00:10:42 +0300

    fix matriel ui icon input

--- packages/elements-material-ui/src/InputIcon/index.tsx
+++ packages/elements-material-ui/src/InputIcon/index.tsx
@@ -4,8 +4,8 @@ import {
   Input as MaterialInputIcon,
   InputAdornment,
   InputProps as MaterialInputIconProps,
-  TextField as MaterialTextField,
-  TextFieldProps as MaterialTextFieldProps,
+  TextField as MaterialTextFieldIcon,
+  TextFieldProps as MaterialTextFieldIconProps,
 } from '@material-ui/core';
 import classNames from 'classnames';
 import './style.scss';
@@ -14,7 +14,7 @@ const materialInputIconMapper = (props: InputIconProps): MaterialInputIconProps 
   const { inForm, fullWidth, labelButton, error, prefix, suffix, className, ...rest } = props;
   const data = {
     ...rest,
-    className: classNames('fe-input__in-form', className),
+    className: classNames('fe-input', props.className),
     fullWidth,
     helperText: error,
     error,
@@ -31,9 +31,42 @@ const materialInputIconMapper = (props: InputIconProps): MaterialInputIconProps 
   return data;
 };
 
+const materialTextFieldIconMapper = (props: InputIconProps): MaterialTextFieldIconProps => {
+  const { inForm, fullWidth, labelButton, error, prefix, suffix, className, ...rest } = props;
+  const data = {
+    ...rest,
+    className: classNames('fe-input__in-form', props.className),
+    fullWidth,
+    helperText: error,
+    error,
+  } as any;
+
+  if (prefix) {
+    data.InputProps = { startAdornment:
+                          (<InputAdornment position='start'>
+                            {prefix}
+                          </InputAdornment>)};
+  } else if (suffix) {
+    data.InputProps = { endAdornment:
+                          (<InputAdornment position='end'>
+                            {suffix}
+                          </InputAdornment>)};
+  }
+  return data;
+};
+
+
 export class InputIcon extends React.Component<InputIconProps> {
   render() {
-    const { children, ...rest } = this.props;
+    const { children,inForm, ...rest } = this.props;
+    if (inForm) {
+      const inputProps = materialTextFieldIconMapper(rest);
+      return <MaterialTextFieldIcon {...inputProps}>{children}</MaterialTextFieldIcon>;
+    } else {
+      const inputProps = materialInputIconMapper(rest);
+      return <MaterialInputIcon {...inputProps}>{children}</MaterialInputIcon>;
+    }
+
     const InputIconProps = materialInputIconMapper(rest);
     return <MaterialInputIcon {...InputIconProps}>{children}</MaterialInputIcon>;
   }
