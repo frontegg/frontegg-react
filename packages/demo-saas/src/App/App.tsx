64dbc96bcf22b26e1a35b96e681c58fd9341e669
Sergii Solonyna <sergeysolonina@gmail.com>
2020-11-12 11:48:52 +0200

    perf(demo): optimize code for building menu

--- packages/demo-saas/src/App/App.tsx
+++ packages/demo-saas/src/App/App.tsx
@@ -1,9 +1,8 @@
 import React, { FC } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
-import { ProtectedRoute, Profile, SSO, MFA, useAuthUser, Team } from '@frontegg/react-auth';
+import { ProtectedRoute, Profile, SSO, useAuthUser, Team } from '@frontegg/react-auth';
 import { PageTabProps, useT } from '@frontegg/react-core';
 import { ElementsPage } from '../Elements/ElementsPage';
-import { withFrontegg } from '../withFrontegg';
 import { PopupExample } from '../PopupExample';
 import { TableExample } from '../TableExample';
 import { ComponentsPage2 } from '../ComponentsPage2';
@@ -18,15 +17,22 @@ const TestPage: FC = () => {
   return <div>{JSON.stringify(user)}</div>;
 };
 
-const MyTab: FC & PageTabProps = () => {
-  return <div>My Tab</div>;
-};
-MyTab.Title = (() => {
-  const { t } = useT();
-  return <>{t('common.enabled')}</>;
-}) as FC;
-MyTab.route = '/my-tab';
-
+const menus = [
+  { to: '/profile', title: 'Profile', component: Profile.Page },
+  { to: '/team', title: 'Team', component: Team.Page },
+  { to: '/sso', title: 'SSO', children: <SSO.Page /> },
+  { to: '/test-auth-user', title: 'Test Auth User', component: TestPage },
+  { to: '/popup', title: 'Popup Examples', component: PopupExample },
+  { to: '/table', title: 'Table Examples', component: TableExample },
+  { to: '/select', title: 'Select Examples', component: SelectorExample },
+  { to: '/connectivity', title: 'Integrations', children: <IntegrationsPage rootPath='/connectivity' /> },
+  { to: '/webhook', title: 'Integrations', children: <IntegrationsPage rootPath='/connectivity' /> },
+  { to: '/notifications', title: 'Notifications Example', children: <NotificationsExample /> },
+  { to: '/components2', component: ComponentsPage2, exact: true },
+  { to: '/components', component: ElementsPage },
+  { to: '/grids', component: GridExamples },
+  { to: '/dialog', component: DialogExample },
+];
 class App extends React.Component<any> {
   render() {
     return (
@@ -34,53 +40,22 @@ class App extends React.Component<any> {
         <Switch>
           <Route exact path='/'>
             <div>
-              <div>
-                <Link to='/profile'>Profile</Link>
-              </div>
-              <div>
-                <Link to='/team'>Team</Link>
-              </div>
-              <div>
-                <Link to='/sso'>SSO</Link>
-              </div>
-              <div>
-                <Link to='/test-auth-user'>Test Auth User</Link>
-              </div>
-              <div>
-                <Link to='/popup'>Popup Examples</Link>
-              </div>
-              <div>
-                <Link to='/table'>Table Examples</Link>
-              </div>
-              <div>
-                <Link to='/select'>Select Examples</Link>
-              </div>
-              <div>
-                <Link to='/connectivity'>Integrations</Link>
-                <Link to='/notifications'>Notifications Example</Link>
-              </div>
+              {menus.map(
+                ({ to, title }, idx) =>
+                  title && (
+                    <div>
+                      <Link to={to}>{title}</Link>{' '}
+                    </div>
+                  )
+              )}
             </div>
           </Route>
-          <Route path='/test-auth-user' component={TestPage} />
-          <Route exact path='/components2' component={ComponentsPage2} />
-          <Route path='/components' component={ElementsPage} />
-          <ProtectedRoute path='/test' />
-          <Route path='/sso'>
-            <SSO.Page />
-          </Route>
-          <Route path='/notifications'>
-            <NotificationsExample />
-          </Route>
+          {menus.map(({ to, component, children, exact }, idx) => (
+            <Route key={idx} path={to} component={component} exact={exact}>
+              {children}
+            </Route>
+          ))}
           <ProtectedRoute path='/profile' component={Profile.Page} />
-          <Route path='/team' component={Team.Page} />
-          <Route exact path='/popup' component={PopupExample} />
-          <Route exact path='/table' component={TableExample} />
-          <Route exact path='/select' component={SelectorExample} />
-          <Route exact path='/grids' component={GridExamples} />
-          <Route path='/connectivity'>
-            <IntegrationsPage rootPath='/connectivity' />
-          </Route>
-          <Route exact path='/dialog' component={DialogExample} />
         </Switch>
       </div>
     );
