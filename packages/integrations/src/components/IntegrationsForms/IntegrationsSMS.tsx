d1bccd7ba0763fcb7ac851691198cdcc52e11d41
Sergii Solonyna <sergeysolonina@gmail.com>
2020-11-13 13:56:18 +0200

    feat(integrations): add the SMS form configuration

--- packages/integrations/src/components/IntegrationsForms/IntegrationsSMS.tsx
+++ packages/integrations/src/components/IntegrationsForms/IntegrationsSMS.tsx
@@ -1,47 +1,29 @@
-import React, { FC, useEffect, useMemo } from 'react';
+import React, { FC, useMemo } from 'react';
 import {
   FInput,
-  Loader,
   FFormik,
   TableColumnProps,
   FCheckbox,
   useDispatch,
   Table,
   useSelector,
+  NotFound,
+  FormikAutoSave,
+  useT,
 } from '@frontegg/react-core';
 import { filterCategories } from '../../utils';
 import { IIntegrationsComponent, IPluginState } from '../../interfaces';
-import { SelectSlack } from '../../elements/SelectSlack';
-
-const smsData = {
-  _id: '5fa003109712b2002aed6290',
-  vendorId: '56303562-1554-42e7-a3bc-d59e3ccd406b',
-  tenantId: 'my-tenant-id',
-  smsSubscriptions: [
-    {
-      isActive: true,
-      smsEvents: [
-        { eventKey: 'general.status', title: 'qwe', message: 'we', channelIds: ['C0167PM6Y87', 'C016UMCDYMS'] },
-      ],
-    },
-  ],
-};
+import { FIntegrationCheckBox } from '../../elements/IntegrationCheckBox';
 
 export const IntegrationsSMS: FC<IIntegrationsComponent> = () => {
+  const { t } = useT();
   const dispatch = useDispatch();
 
-  const { isLoading, categories, channelMap, sms } = useSelector(
-    ({
-      integrations: {
-        slackChannels: { isLoading },
-        categories,
-        channelMap,
-        sms,
-      },
-    }: IPluginState) => ({
-      isLoading,
-      categories,
+  const { categories, channelMap, sms, isSaving } = useSelector(
+    ({ integrations: { sms, isSaving, categories, channelMap } }: IPluginState) => ({
       sms,
+      isSaving,
+      categories,
       channelMap: channelMap?.sms,
     })
   );
@@ -55,11 +37,11 @@ export const IntegrationsSMS: FC<IIntegrationsComponent> = () => {
         name,
         events: events?.map(({ id, displayName, key }: any) => ({
           displayName,
-          ...smsData.smsSubscriptions.find(({ smsEvents }) => smsEvents.some(({ eventKey }) => eventKey === key)),
+          // ...smsData.smsSubscriptions.find(({ smsEvents }) => smsEvents.some(({ eventKey }) => eventKey === key)),
           id,
         })),
       })),
-    [cleanCategory, smsData.smsSubscriptions]
+    [cleanCategory, sms]
   );
 
   const columns = useMemo(
@@ -73,89 +55,36 @@ export const IntegrationsSMS: FC<IIntegrationsComponent> = () => {
             },
             {
               accessor: 'isActive',
-              Header: 'ENABLED',
-              Cell: ({ row: { index } }) => (
-                <FCheckbox
-                  inForm
-                  className='fe-integrations-checkbox'
-                  name={`values[${idx}].events[${index}].isActive`}
-                />
-              ),
+              Header: t('common.enable').toUpperCase(),
+              Cell: ({ row: { index } }) => <FIntegrationCheckBox name={`data[${idx}].events[${index}].isActive`} />,
             },
             {
               accessor: 'smsEvents',
-              Header: 'CHANNELS',
-              Cell: () => <SelectSlack name={`values[${idx}].events[0].sms`} />,
+              Header: t('common.channels').toUpperCase(),
+              Cell: () => <FInput name={`values[${idx}].events[0].sms`} />,
             },
             {
               accessor: 'non',
-              Header: 'MESSAGE',
-              // Cell: ({ value }) => <InputDebounce value={value?.message || ''} onChange={(v) => console.log(v)} />,
-              Cell: ({ row: { index }, ...val }) => {
-                // console.log( });
+              Header: t('common.message').toUpperCase(),
+              Cell: ({ row: { index } }) => {
                 return <FInput name={`values[${idx}].events[${index}].message`} />;
               },
             },
           ] as TableColumnProps<{}>[]
       ),
-    [tablesData]
+    [tablesData, t]
   );
 
-  useEffect(() => {
-    // dispatch(integrationsActions.loadSlackActions());
-    return () => {
-      // dispatch(integrationsActions.cleanSlackData());
-    };
-  }, [dispatch]);
-
-  return isLoading ? (
-    <Loader center />
-  ) : tablesData ? (
+  return tablesData ? (
     <FFormik.Formik initialValues={{ values: tablesData }} onSubmit={(val) => console.log(val)}>
       <FFormik.Form>
+        <FormikAutoSave isSaving={isSaving} />
         {(tablesData || []).map(({ id, events }, idx) => (
           <Table rowKey='id' key={id} columns={columns[idx]} data={events || []} totalData={events?.length || 0} />
         ))}
       </FFormik.Form>
     </FFormik.Formik>
   ) : (
-    <> Required configure the connectivity</>
+    <NotFound />
   );
-
-  // const { t } = useT();
-  // const validationSchema = validateSchema({
-  //   to: validationPhone(t),
-  // });
-  // return (
-  //   <IntegrationsForm validationSchema={validationSchema} onClose={onClose} type='sms' initialValues={{ to: [''] }}>
-  //     {({ values: { to }, setFieldValue }: FormikProps<Omit<ISMSConfigurations, 'id'>>) => (
-  //       <>
-  //         {to.map((_, idx) => (
-  //           <React.Fragment key={idx}>
-  //             <Grid item xs={1}>
-  //               {idx === 0 && t('integrations.sms.to')}
-  //             </Grid>
-  //             <Grid item xs={9}>
-  //               <FInput name={`to[${idx}]`} placeholder='+123456789012' />
-  //             </Grid>
-  //             <Grid item xs={2}>
-  //               <Button
-  //                 disabled={to.length === 1}
-  //                 onClick={() => setFieldValue('to', [...to.slice(0, idx), ...to.slice(idx + 1)])}
-  //               >
-  //                 -
-  //               </Button>
-  //             </Grid>
-  //           </React.Fragment>
-  //         ))}
-  //         <Grid item xs={10}>
-  //           &nbsp;
-  //         </Grid>
-  //         <Grid item xs={2}>
-  //           <Button onClick={() => setFieldValue('to', [...to, ''])}>+</Button>
-  //         </Grid>
-  //       </>
-  //     )}
-  //   </IntegrationsForm>
-  // );
 };
