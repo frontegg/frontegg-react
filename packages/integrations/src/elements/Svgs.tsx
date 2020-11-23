20665ebc611b488a1b70739395d53d8c5bb86593
Sergii Solonyna <sergeysolonina@gmail.com>
2020-11-23 16:41:01 +0200

    fix(integrations): fix the email icon and some styles

--- packages/integrations/src/elements/Svgs.tsx
+++ packages/integrations/src/elements/Svgs.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 export const EmailSvg: FC<React.SVGProps<SVGSVGElement>> = (props) => {
   const {
     xmlns = 'http://www.w3.org/2000/svg',
-    viewBox = '0 0 512 512',
+    viewBox = '0 0 24 24',
     className,
     width = '2rem',
     height = '2rem',
@@ -19,9 +19,10 @@ export const EmailSvg: FC<React.SVGProps<SVGSVGElement>> = (props) => {
       xmlns={xmlns}
       className={classNames('fe-icon', className)}
     >
+      <path d='M0 0h24v24H0z' fill='none' />
       <path
         fill='#fa6400'
-        d='M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z'
+        d='M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z'
       />
     </svg>
   );
