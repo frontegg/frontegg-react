28dc5917cdbcbf839c10db0e811d0518ee285398
Sergii Solonyna <sergeysolonina@gmail.com>
2020-11-23 15:49:55 +0200

    feat(integrations): add icons to the list of platforms

--- packages/integrations/src/consts.ts
+++ packages/integrations/src/consts.ts
@@ -2,6 +2,7 @@ import { api, IEmailSMSConfigResponse, ISlackConfigurations, IWebhooksConfigurat
 import { FC } from 'react';
 import { IntegrationsSlack, IntegrationsSMS, IntegratorsEmail } from './components/IntegrationsForms';
 import { IntegrationsWebhooks } from './components/IntegrationsWebhooks';
+import { BellSvg, EmailSvg, SlackSvg, SmsSvg } from './elements/Svgs';
 import { TPlatform, IIntegrationsComponent } from './interfaces';
 
 export const type2ApiGet: Record<TPlatform | 'categories' | 'channelMap', any> = {
@@ -30,27 +31,32 @@ export const channels2Platform: Record<
     title: string;
     events(data: IEmailSMSConfigResponse[] | ISlackConfigurations | IWebhooksConfigurations[]): number;
     isActive(data: IEmailSMSConfigResponse[] | ISlackConfigurations | IWebhooksConfigurations[]): boolean;
+    image: FC<React.SVGProps<SVGSVGElement>>;
   }
 > = {
   sms: {
     title: 'integrations.sms',
     events: (data) => (data as IEmailSMSConfigResponse[])?.length || 0,
     isActive: (data) => !!(data as IEmailSMSConfigResponse[])?.length,
+    image: SmsSvg,
   },
   email: {
     title: 'common.email',
     events: (data) => (data as IEmailSMSConfigResponse[])?.length || 0,
     isActive: (data) => !!(data as IEmailSMSConfigResponse[])?.length,
+    image: EmailSvg,
   },
   slack: {
     title: 'integrations.slack',
     events: (data) => (data as ISlackConfigurations)?.slackSubscriptions?.length || 0,
     isActive: (data) => !!(data as ISlackConfigurations)?.slackSubscriptions,
+    image: SlackSvg,
   },
   webhook: {
     title: 'integrations.webhook',
     events: (data) => (data as IWebhooksConfigurations[])?.length || 0,
     isActive: (data) => !!(data as IWebhooksConfigurations[])?.length,
+    image: BellSvg,
   },
 };
 
