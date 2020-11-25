15315895be3731f36ddc3792b92fbef2e1f43b39
David Antoon <davidmantoon@gmail.com>
2020-11-25 16:30:39 +0200

    refactor(auth): add missing useEffect import

--- packages/auth/src/SocialLogins/SocialLogins.tsx
+++ packages/auth/src/SocialLogins/SocialLogins.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, ReactNode, useEffect } from 'react';
 import { Loader } from '@frontegg/react-core';
 import { useAuth } from '../hooks';
 import { AuthState } from '../Api';
