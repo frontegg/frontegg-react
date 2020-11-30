85dc7b7f8ca01a7ac6eb17a2f735f1bfd9c68bee
David Antoon <davidmantoon@gmail.com>
2020-11-30 13:30:45 +0200

    build: update alpha version before release

--- scripts/update-prerelease-version.js
+++ scripts/update-prerelease-version.js
@@ -5,9 +5,13 @@ function updatePreReleaseVersion(packagePath, nextVersion) {
   if (packagePath.indexOf('demo-saas') !== -1) {
     return;
   }
-  const pkg = JSON.parse(fs.readFileSync(`${packagePath}/dist/package.json`, { encoding: 'utf8' }));
+  const pkg = JSON.parse(fs.readFileSync(`${packagePath}/package.json`, { encoding: 'utf8' }));
+  const distPkg = JSON.parse(fs.readFileSync(`${packagePath}/dist/package.json`, { encoding: 'utf8' }));
+  const lernaJSON = JSON.parse(fs.readFileSync(path.join(`${packagePath}/../../lerna.json`), { encoding: 'utf8' }));
 
   pkg.version = nextVersion;
+  distPkg.version = nextVersion;
+  lernaJSON.version = nextVersion;
 
   [
     '@frontegg/rest-api',
@@ -21,9 +25,26 @@ function updatePreReleaseVersion(packagePath, nextVersion) {
     if (pkg.dependencies[dep]) {
       pkg.dependencies[dep] = `${nextVersion}`;
     }
+    if (distPkg.dependencies[dep]) {
+      distPkg.dependencies[dep] = `${nextVersion}`;
+    }
+    if (pkg.devDependencies[dep]) {
+      pkg.devDependencies[dep] = `${nextVersion}`;
+    }
+    if (distPkg.devDependencies[dep]) {
+      distPkg.devDependencies[dep] = `${nextVersion}`;
+    }
+    if (pkg.peerDependencies[dep]) {
+      pkg.peerDependencies[dep] = `${nextVersion}`;
+    }
+    if (distPkg.peerDependencies[dep]) {
+      distPkg.peerDependencies[dep] = `${nextVersion}`;
+    }
   });
 
-  fs.writeFileSync(`${packagePath}/dist/package.json`, JSON.stringify(pkg, null, 2), { encoding: 'utf-8' });
+  fs.writeFileSync(`${packagePath}/package.json`, JSON.stringify(pkg, null, 2), { encoding: 'utf-8' });
+  fs.writeFileSync(`${packagePath}/dist/package.json`, JSON.stringify(distPkg, null, 2), { encoding: 'utf-8' });
+  fs.writeFileSync(path.join(`${packagePath}/../../lerna.json`), JSON.stringify(lernaJSON, null, 2), { encoding: 'utf-8' });
 }
 
 updatePreReleaseVersion(process.argv[2], process.argv[3]);
