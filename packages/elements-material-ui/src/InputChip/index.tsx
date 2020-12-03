8f6404aa9cb659512c17ce3ec7b03e48b6f0f2e4
Sergii Solonyna <sergeysolonina@gmail.com>
2020-12-03 16:39:44 +0200

    fix(elements): fix styles for the InputChip component in the material library

--- packages/elements-material-ui/src/InputChip/index.tsx
+++ packages/elements-material-ui/src/InputChip/index.tsx
@@ -1,18 +1,25 @@
 import { IInputChip } from '@frontegg/react-core';
 import { Chip, Grid } from '@material-ui/core';
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 
-export const InputChip: FC<IInputChip> = ({ chips, inputValue, onDelete, ...inputProps }) => (
-  <div>
-    <Grid container>
-      {chips.map((chip, idx) => (
+import './styles.scss';
+
+export const InputChip: FC<IInputChip> = ({ chips, label, inputValue, onDelete, error, ...inputProps }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <div className='MuiChipInput' onClick={() => inputRef.current && inputRef.current.focus()}>
+      {!!label && <div>{label}</div>}
+      <Grid container alignItems='center'>
+        {chips.map((chip, idx) => (
+          <Grid item>
+            <Chip label={chip} onDelete={() => onDelete(idx)} />
+          </Grid>
+        ))}
         <Grid item>
-          <Chip label={chip} onDelete={() => onDelete(idx)} />
+          <input {...inputProps} value={inputValue} ref={inputRef} />
         </Grid>
-      ))}
-      <Grid item>
-        <input {...inputProps} value={inputValue} />
       </Grid>
-    </Grid>
-  </div>
-);
+      {error && <div className='MuiAlert-standardError'>{error}</div>}
+    </div>
+  );
+};
