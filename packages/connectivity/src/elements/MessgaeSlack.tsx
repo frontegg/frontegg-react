6314a4ea2eb1be8b1a0043077de3ceb4f410bd68
Sergii Solonyna <sergeysolonina@gmail.com>
2020-12-10 10:46:35 +0200

    fix(connectivity): fix the enabled edit data form the SMS and Email webhooks

--- packages/connectivity/src/elements/MessgaeSlack.tsx
+++ packages/connectivity/src/elements/MessgaeSlack.tsx
@@ -1,13 +1,8 @@
 import { FFormik, FInput } from '@frontegg/react-core';
 import React, { FC } from 'react';
-import { ISlackTableData } from '../interfaces';
+import { IFormikEditComponent, ISlackTableData } from '../interfaces';
 
-export interface IMessageSlack {
-  eventIdx: number;
-  dataIdx: number;
-}
-
-export const MessageSlack: FC<IMessageSlack> = ({ eventIdx, dataIdx }) => {
+export const MessageSlack: FC<IFormikEditComponent> = ({ eventIdx, dataIdx }) => {
   const { values } = FFormik.useFormikContext<{ data: ISlackTableData[] }>();
 
   const { isActive } = values.data[dataIdx].events[eventIdx];
