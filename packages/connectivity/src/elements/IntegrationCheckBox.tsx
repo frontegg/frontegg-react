2eb845d4a568272cf9b859b45e606d690ab4b861
Sergii Solonyna <sergeysolonina@gmail.com>
2021-01-11 18:50:23 +0200

    feat(connectivity): replace checkboxes to the SwitchToggle components

--- packages/connectivity/src/elements/IntegrationCheckBox.tsx
+++ packages/connectivity/src/elements/IntegrationCheckBox.tsx
@@ -1,7 +1,5 @@
 import React, { FC, InputHTMLAttributes } from 'react';
-import classnames from 'classnames';
-import { FFormik } from '@frontegg/react-core';
-import { CheckSvg } from './Svgs';
+import { FFormik, SwitchToggle, SwitchToggleProps } from '@frontegg/react-core';
 
 export interface IIntegrationCheckBox {
   name: string;
@@ -10,18 +8,7 @@ export interface IIntegrationCheckBox {
 export const FIntegrationCheckBox: FC<IIntegrationCheckBox> = ({ name }) => {
   const [{ value, ...inputProps }, {}, { setValue }] = FFormik.useField(name);
 
-  return <IntegrationCheckBox {...inputProps} name={name} checked={!!value} onChange={(e) => setValue(!value)} />;
+  return <IntegrationCheckBox {...inputProps} name={name} value={!!value} onChange={(e) => setValue(!value)} />;
 };
 
-export const IntegrationCheckBox: FC<Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'size'>> = ({
-  checked,
-  onChange,
-  ...inputProps
-}) => (
-  <label className={classnames('fe-integration-checkbox', { active: !!checked })}>
-    <CheckSvg
-      className={classnames(`fe-connectivity-platform-check`, { [`fe-connectivity-platform-check-active`]: !!checked })}
-    />
-    <input {...inputProps} type='checkbox' checked={checked} onChange={onChange} />
-  </label>
-);
+export const IntegrationCheckBox: FC<SwitchToggleProps> = (props) => <SwitchToggle {...props} />;
