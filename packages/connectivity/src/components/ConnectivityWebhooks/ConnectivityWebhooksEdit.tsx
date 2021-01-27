0b11327f3561bc41efc14acac7742b831aad2e21
Sergii Solonyna <sergeysolonina@gmail.com>
2021-01-27 11:03:54 +0200

    fix(connectivity): fix empty value for the security property in the wehook component

--- packages/connectivity/src/components/ConnectivityWebhooks/ConnectivityWebhooksEdit.tsx
+++ packages/connectivity/src/components/ConnectivityWebhooks/ConnectivityWebhooksEdit.tsx
@@ -1,11 +1,10 @@
 import React, { FC, useCallback, useLayoutEffect, useMemo } from 'react';
-import { Button, Icon, Tabs, usePrevious, useSelector, useT } from '@frontegg/react-core';
+import { Icon, Tabs, usePrevious, useSelector, useT } from '@frontegg/react-core';
 import { useHistory } from 'react-router-dom';
 import { IPluginState } from '../../interfaces';
 import { ConnectivityWebhooksForm } from './ConnectivityWebhooksForm';
 import { ConnectivityWebhooksLog } from './ConnectivityWebhooksLog';
 import { IWebhookLocationState } from './interfaces';
-import classNames from 'classnames';
 
 const itemsArray = ['common.detail', 'common.logs'];
 
@@ -26,7 +25,7 @@ export const ConnectivityWebhooksEdit: FC = () => {
 
   const prevIsSaving = usePrevious(isSaving);
 
-  const data = webhook?.find(({ _id }) => _id === locationState.id);
+  const data = useMemo(() => webhook?.find(({ _id }) => _id === locationState.id), [webhook, locationState]);
 
   const onBack = useCallback(() => {
     historyReplace({ ...location, state: { ...locationState, view: 'list', id: undefined } });
