eebdd710199c505f927d8446079f70c22f25909b
David Antoon <david@frontegg.com>
2021-02-02 14:11:49 +0200

    feat(auth): Auto login after activate account succeeded

--- packages/auth/src/ActivateAccount/ActivateAccountSuccessRedirect.tsx
+++ packages/auth/src/ActivateAccount/ActivateAccountSuccessRedirect.tsx
@@ -9,17 +9,21 @@ export interface ActivateAccountSuccessRedirectProps {
 export const ActivateAccountSuccessRedirect: FC<ActivateAccountSuccessRedirectProps> = (props) => {
   const { renderer } = props;
   const { t } = useT();
-  const { requestAuthorize, resetActivateState } = useAuth(({ routes, onRedirectTo }) => ({ routes, onRedirectTo }));
+  const { requestAuthorize, resetActivateState, onRedirectTo, routes } = useAuth(({ routes, onRedirectTo }) => ({
+    routes,
+    onRedirectTo,
+  }));
 
   if (renderer) {
     return renderer(omitProps(props, ['renderer']));
   }
   useEffect(() => {
     setTimeout(() => {
-      resetActivateState();
       requestAuthorize(true);
+      onRedirectTo(routes.authenticatedUrl);
     }, 1000);
-  }, []);
+    return resetActivateState as () => void;
+  }, [resetActivateState, requestAuthorize, onRedirectTo]);
 
   return (
     <>
--- packages/auth/src/Api/ActivateState/saga.ts
+++ packages/auth/src/Api/ActivateState/saga.ts
@@ -8,13 +8,7 @@ function* activateAccount({ payload }: PayloadAction<IActivateAccount>) {
   yield put(actions.setActivateState({ loading: true }));
   try {
     yield call(api.auth.activateAccount, payload);
-    yield put(
-      actions.setState({
-        isAuthenticated: false,
-        user: null,
-        activateState: { loading: false, error: undefined, step: ActivateStep.success },
-      })
-    );
+    yield put(actions.setActivateState({ loading: false, error: undefined, step: ActivateStep.success }));
   } catch (e) {
     yield put(actions.setActivateState({ loading: false, error: e.message }));
   }
