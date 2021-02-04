79dd17267da92c1d8bb651fe6210d3c6b5b42519
David Antoon <david@frontegg.com>
2021-02-04 19:40:45 +0200

    feat: Add option to inject SSO components without routes

--- packages/auth/src/SSO/index.ts
+++ packages/auth/src/SSO/index.ts
@@ -3,9 +3,9 @@ import { SSOPage } from './SSOPage';
 import { SSOHeader } from './SSOHeader';
 import { SSOToggle } from './SSOToggle';
 import { SSOOverviewPage, SSOSteps, SSONoDataPlaceholder } from './SSOOverviewPage';
-import { SSOClaimDomainPage, SSOClaimDomainGuide, SSOClaimDomainForm } from './SSOClaimDomainPage';
-import { SSOConfigureIDPPage } from './SSOConfigureIDPPage';
-import { SSOManageAuthorizationPage } from './SSOManageAuthorizationPage';
+import { SSOClaimDomainComponent, SSOClaimDomainPage, SSOClaimDomainGuide, SSOClaimDomainForm } from './SSOClaimDomainPage';
+import { SSOConfigureIDPComponent, SSOConfigureIDPPage } from './SSOConfigureIDPPage';
+import { SSOManageAuthorizationComponent, SSOManageAuthorizationPage } from './SSOManageAuthorizationPage';
 
 export const SSO = {
   Page: SSOPage,
@@ -14,8 +14,11 @@ export const SSO = {
   Toggle: SSOToggle,
   OverviewPage: SSOOverviewPage,
   ClaimDomainPage: SSOClaimDomainPage,
+  ClaimDomainComponent: SSOClaimDomainComponent,
   ConfigureIDPPage: SSOConfigureIDPPage,
+  ConfigureIDPComponent: SSOConfigureIDPComponent,
   ManageAuthorizationPage: SSOManageAuthorizationPage,
+  ManageAuthorizationComponent: SSOManageAuthorizationComponent,
   Steps: SSOSteps,
   NoDataPlaceholder: SSONoDataPlaceholder,
   ClaimDomainGuide: SSOClaimDomainGuide,
