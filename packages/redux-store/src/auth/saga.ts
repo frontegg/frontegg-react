ae3a366633d1ab7f502437bec9413942e902104b
David Antoon <david@frontegg.com>
2021-04-12 17:38:56 +0300

    fix: remove live sagas from mock generator function
    FR-2391

--- packages/redux-store/src/auth/saga.ts
+++ packages/redux-store/src/auth/saga.ts
@@ -4,7 +4,7 @@ import { mfaSagas, mfaSagasMock } from './MfaState/saga';
 import { forgotPasswordSagas } from './ForgotPasswordState/saga';
 import { activateSagas } from './ActivateState/saga';
 import { acceptInvitationSagas } from './AcceptInvitationState/saga';
-import { loginSagas } from './LoginState/saga';
+import { loginSagas, loginSagasMock } from './LoginState/saga';
 import { teamSagas, teamSagasMock } from './TeamState/saga';
 import { socialLoginsSaga } from './SocialLogins/saga';
 import { signUpSaga } from './SignUp/saga';
@@ -37,12 +37,12 @@ export function* sagas() {
 
 export function* mockSagas() {
   yield all([
-    call(loginSagas),
-    call(activateSagas),
-    call(acceptInvitationSagas),
-    call(forgotPasswordSagas),
-    call(socialLoginsSaga),
-    call(signUpSaga),
+    call(loginSagasMock),
+    // call(activateSagas),
+    // call(acceptInvitationSagas),
+    // call(forgotPasswordSagas),
+    // call(socialLoginsSaga),
+    // call(signUpSaga),
     call(ssoSagasMock),
     call(profileSagasMock),
     call(mfaSagasMock),
@@ -51,6 +51,6 @@ export function* mockSagas() {
     call(securityPolicySagasMock),
     call(accountSettingsSagaMock),
     call(tenantsSagasMock),
-    call(rolesSagas),
+    // call(rolesSagas),
   ]);
 }
