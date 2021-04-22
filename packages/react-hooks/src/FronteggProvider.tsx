b4ed38bb603c66ca7199847c475355d168454ca8
David Antoon <david@frontegg.com>
2021-04-22 15:58:01 +0300

    - add option to use admin box with custom login box - prevent displayed children if loginBox routes presented
    #FR-2645

--- packages/react-hooks/src/FronteggProvider.tsx
+++ packages/react-hooks/src/FronteggProvider.tsx
@@ -1,8 +1,8 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useEffect, useMemo, useRef, useState } from 'react';
 import { Provider } from 'react-redux';
 import { createFronteggStore } from '@frontegg/redux-store';
 import { ContextHolder, ContextOptions } from '@frontegg/rest-api';
-import { useAuth } from './auth';
+import { useAuth, useAuthRoutes } from './auth';
 
 type FronteggProviderProps = {
   app?: any; // FronteggApp
@@ -21,27 +21,78 @@ const setLoading = (loading: boolean): void => {
     }
   }
 };
-const FronteggContent: FC<{ setLoading: (loading: boolean) => void }> = ({ children, setLoading }) => {
+
+const HideChildrenIfFronteggRoutes: FC<{ basename?: string }> = ({ children, basename }) => {
+  const routes = useAuthRoutes();
+  const uriRef = useRef(window.location.pathname);
+  const [uri, setUri] = useState(window.location.pathname);
+  useEffect(() => {
+    document.addEventListener('frontegg_onRedirectTo_fired', () => {
+      if (uriRef.current !== window.location.pathname) {
+        uriRef.current = window.location.pathname;
+        setUri(document.location.pathname);
+      }
+    });
+    window.addEventListener('popstate', () => {
+      if (uriRef.current !== window.location.pathname) {
+        uriRef.current = window.location.pathname;
+        setUri(document.location.pathname);
+      }
+    });
+
+    setInterval(() => {
+      if (uriRef.current !== window.location.pathname) {
+        uriRef.current = window.location.pathname;
+        setUri(document.location.pathname);
+      }
+    }, 50);
+  }, [setUri]);
+
+  const calculatedBasename = basename ? (basename.endsWith('/') ? basename.substring(0, basename.length - 1) : '') : '';
+  switch (uri) {
+    case calculatedBasename + routes.loginUrl:
+    case calculatedBasename + routes.logoutUrl:
+    case calculatedBasename + routes.activateUrl:
+    case calculatedBasename + routes.acceptInvitationUrl:
+    case calculatedBasename + routes.forgetPasswordUrl:
+    case calculatedBasename + routes.resetPasswordUrl:
+    case calculatedBasename + routes.socialLoginCallbackUrl:
+    case calculatedBasename + routes.signUpUrl:
+      return null;
+  }
+
+  return <>{children}</>;
+};
+
+const FronteggContent: FC<{ app?: any; setLoading: (loading: boolean) => void }> = ({ children, app, setLoading }) => {
   const { isLoading } = useAuth(({ isLoading }) => ({ isLoading }));
+  const customLoginBox: boolean = app ? !!app.options?.customLoginBox : true;
   setLoading(isLoading);
   if (isLoading) {
     return null;
   }
-  return <>{children}</>;
+  if (customLoginBox) {
+    return <>{children}</>;
+  }
+
+  return <HideChildrenIfFronteggRoutes basename={app?.options.basename}>{children}</HideChildrenIfFronteggRoutes>;
 };
+
 export const FronteggProvider: FC<FronteggProviderProps> = ({
   children,
   app,
   contextOptions,
   setLoading: setLoadingProps,
 }) => {
-  const context = contextOptions ?? app.options.contextOptions;
+  const context = app?.options?.contextOptions ?? contextOptions;
   ContextHolder.setContext(context);
   const store = useMemo(() => createFronteggStore({ context }, app), [app]);
 
   return (
     <Provider store={store}>
-      <FronteggContent setLoading={setLoadingProps ?? setLoading}>{children}</FronteggContent>
+      <FronteggContent setLoading={setLoadingProps ?? setLoading} app={app}>
+        {children}
+      </FronteggContent>
     </Provider>
   );
 };
