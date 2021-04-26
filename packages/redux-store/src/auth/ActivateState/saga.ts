84db2376901b34f53d7079108d989b7d1c5d57b7
David Antoon <david@frontegg.com>
2021-04-26 21:14:35 +0300

    fix: disable refresh token after activate user
    #FR-2761

--- packages/redux-store/src/auth/ActivateState/saga.ts
+++ packages/redux-store/src/auth/ActivateState/saga.ts
@@ -1,14 +1,25 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { call, put, takeLeading } from 'redux-saga/effects';
-import { api, IActivateAccount, IResendActivationEmail } from '@frontegg/rest-api';
+import { call, put, select, takeLeading } from 'redux-saga/effects';
+import { api, ContextHolder, IActivateAccount, IResendActivationEmail } from '@frontegg/rest-api';
 import { actions } from '../reducer';
 import { ActivateAccountStep } from './interfaces';
+import { afterAuthNavigation, getMfaRequiredState, isMfaRequired } from '../LoginState/saga';
 
 function* activateAccount({ payload }: PayloadAction<IActivateAccount>) {
   yield put(actions.setActivateState({ loading: true }));
   try {
-    yield call(api.auth.activateAccount, payload);
-    yield put(actions.setActivateState({ loading: false, error: undefined, step: ActivateAccountStep.success }));
+    const onRedirectTo = ContextHolder.onRedirectTo;
+    const { routes } = yield select((state) => state.auth);
+    const user = yield call(api.auth.activateAccount, payload);
+
+    if (isMfaRequired(user)) {
+      yield put(actions.setState(getMfaRequiredState(user)));
+      onRedirectTo(routes.loginUrl, { preserveQueryParams: true });
+    } else {
+      yield put(actions.loadTenants());
+      yield put(actions.setState({ user, isAuthenticated: true }));
+      yield afterAuthNavigation();
+    }
   } catch (e) {
     yield put(actions.setActivateState({ loading: false, error: e.message }));
   }
