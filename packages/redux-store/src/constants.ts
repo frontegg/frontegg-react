401ee8153927a319e4f73f84c9de802f461bd5eb
Arnaut Max <maximarnayt@gmail.com>
2021-06-14 13:22:32 +0300

    FR-3241: rename audtisStoreName to auditLogs

--- packages/redux-store/src/constants.ts
+++ packages/redux-store/src/constants.ts
@@ -1,4 +1,4 @@
 export const FRONTEGG_AFTER_AUTH_REDIRECT_URL = 'FRONTEGG_AFTER_AUTH_REDIRECT_URL';
 
 export const authStoreName = 'auth';
-export const auditsStoreName = 'audits';
+export const auditsStoreName = 'auditLogs';
