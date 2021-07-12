4f1e80965e9bb9d115b42014b48ae75efb374190
Kouper <israel@frontegg.com>
2021-07-12 13:40:50 +0300

    FR-2456 - create customer on payment config

--- packages/redux-store/src/subscriptions/Config/saga.ts
+++ packages/redux-store/src/subscriptions/Config/saga.ts
@@ -6,61 +6,58 @@ import {
   ICreateStripeCustomerResponse,
   IStripeCustomerResponse,
   IStripePaymentProviderConfigurationResponse,
+  IUserProfile,
 } from '@frontegg/rest-api';
-import { AuthState, ProfileState } from '../../auth';
+import { AuthState } from '../../auth';
 
 export function* configSagas() {
   yield takeEvery(configActions.loadPaymentConfiguration, loadPaymentConfiguration);
 }
 
 function* loadPaymentConfiguration() {
-  const { profile }: ProfileState = yield select((state) => state.auth?.profileState);
   yield put(configActions.setLoading(true));
   try {
-    if (profile) {
-      yield loadStripeCustomer(profile.tenantId);
-    }
     const response: IStripePaymentProviderConfigurationResponse = yield call(
       api.subscriptions.getStripePaymentProviderConfiguration
     );
     yield put(configActions.setStripePaymentProvider(response.publishableKey));
+    yield loadStripeCustomer();
     yield put(configActions.setLoading(false));
   } catch (e) {
     yield put(configActions.setError(e));
   }
 }
 
-function* loadStripeCustomer(tenantId: string) {
-  yield put(configActions.setLoading(true));
-
+function* loadStripeCustomer() {
+  const authState: AuthState = yield select((state) => state.auth);
+  const profile = authState.profileState.profile;
+  if (!profile || !profile.id) {
+    return;
+  }
   try {
     const response: IStripeCustomerResponse = yield call(api.subscriptions.getStripeCustomer, tenantId);
     yield put(configActions.setStripeCustomerId(response.stripeCustomerId));
-    yield put(configActions.setLoading(false));
   } catch (e) {
     if (e instanceof fetch.FronteggApiError && e.statusCode === 404) {
-      yield createCustomer();
+      if (profile && profile.email && profile.name) {
+        yield createCustomer(profile);
+      }
     } else {
       yield put(configActions.setError(e));
     }
   }
 }
 
-function* createCustomer() {
-  const authState: AuthState = yield select((state) => state.auth);
-  const profile = authState.profileState.profile;
-
-  if (profile && profile.email && profile.name) {
-    yield put(configActions.setLoading(true));
-    try {
-      const response: ICreateStripeCustomerResponse = yield call(api.subscriptions.createStripeCustomer, {
-        name: profile.name,
-        email: profile.email,
-      });
-      yield put(configActions.setStripeCustomerId(response.stripeCustomerId));
-      yield put(configActions.setLoading(false));
-    } catch (e) {
-      yield put(configActions.setError(e));
-    }
+function* createCustomer(profile: IUserProfile) {
+  yield put(configActions.setLoading(true));
+  try {
+    const response: ICreateStripeCustomerResponse = yield call(api.subscriptions.createStripeCustomer, {
+      name: profile.name,
+      email: profile.email,
+    });
+    yield put(configActions.setStripeCustomerId(response.stripeCustomerId));
+    yield put(configActions.setLoading(false));
+  } catch (e) {
+    yield put(configActions.setError(e));
   }
 }
