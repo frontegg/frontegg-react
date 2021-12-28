68bdd30170874344cc8b923a82ea1fc2545b452a
Rotem <rotem@frontegg.com>
2021-12-28 15:22:54 +0200

    Edit social login test to work with the refreshTokenV2

--- packages/auth/src/constants.ts
+++ packages/auth/src/constants.ts
@@ -1,2 +1,36 @@
+import { v4 as uuid } from 'uuid';
+import jwtEncode from 'jwt-encode';
+
 export const FRONTEGG_AFTER_AUTH_REDIRECT_URL = 'FRONTEGG_AFTER_AUTH_REDIRECT_URL';
 export const FRONTEGG_CODE_VERIFIER = 'FRONTEGG_CODE_VERIFIER';
+
+const expiresIn = 365000;
+const iat = new Date();
+const expires = new Date(iat.getMilliseconds() + expiresIn);
+const userId = uuid();
+const aud = uuid();
+const token = {
+  sub: userId,
+  name: 'Test User 1',
+  email: 'test+1@frontegg.com',
+  email_verified: true,
+  roles: ['admin'],
+  permissions: ['fe.*'],
+  metadata: {},
+  profilePictureUrl: '',
+  tenantId: 'my-tenant-id',
+  tenantIds: ['my-tenant-id'],
+  type: 'userToken',
+  iat: iat.getMilliseconds(),
+  exp: expires.getMilliseconds(),
+  aud,
+  iss: 'https://test.frontegg.com',
+};
+
+export const refreshTokenResponse = {
+  accessToken: jwtEncode(token, 'secret'),
+  expires,
+  expiresIn,
+  refreshToken: '',
+  mfaRequired: false,
+};
