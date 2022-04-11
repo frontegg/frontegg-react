5aedee1eec7ad66682599910eaa6ac1bd0dc73f4
David Antoon <david@frontegg.com>
2022-04-11 16:14:39 +0300

    increase viewport

--- packages/demo-saas/src/index.tsx
+++ packages/demo-saas/src/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import { App } from './App';
-// import OldApp from './OldApp';
+// import { App } from './App';
+import OldApp from './OldApp';
 import {
-  withFrontegg,
-  // withOldFrontegg
+  // withFrontegg,
+  withOldFrontegg,
 } from './withFrontegg';
 import { BrowserRouter } from 'react-router-dom';
 
-const AppWithFrontegg = withFrontegg(App);
-// const AuditLogsConnectivityApp = withOldFrontegg(OldApp);
+// const AppWithFrontegg = withFrontegg(App);
+const AuditLogsConnectivityApp = withOldFrontegg(OldApp);
 
 ReactDOM.render(
   <BrowserRouter>
-    <AppWithFrontegg />
-    {/*<AuditLogsConnectivityApp />*/}
+    {/*<AppWithFrontegg />*/}
+    <AuditLogsConnectivityApp />
   </BrowserRouter>,
   document.getElementById('root')
 );
