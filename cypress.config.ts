161e743fa68d1db0fd8e6f13fba45f9a68174afb
Piotr Pliszczyński <piotr.pilczynski@hitachi-powergrids.com>
2022-06-21 11:27:32 +0200

    FR-7912 update cypress configs for components

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   experimentalFetchPolyfill: true,
-  experimentalComponentTesting: true,
   experimentalSourceRewriting: true,
   projectId: 'odcj7u',
   viewportHeight: 800,
   viewportWidth: 1440,
+
   component: {
-    setupNodeEvents(on, config) {},
     specPattern: 'packages/**/*.cy-spec.*',
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
   },
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
