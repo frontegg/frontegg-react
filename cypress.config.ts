23022a7194b08e7f27642c306f24a6a671cc3223
Piotr Pliszczyński <piotr.pilczynski@hitachi-powergrids.com>
2022-06-21 16:06:07 +0200

    FR-7912 Update cypress config - remove all experimental flags

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  experimentalFetchPolyfill: true,
-  experimentalSourceRewriting: true,
   projectId: 'odcj7u',
   viewportHeight: 800,
   viewportWidth: 1440,
 
   e2e: {
     specPattern: 'packages/**/*.cy-spec.*',
-    supportFile: false
+    supportFile: false,
+
   },
 });
