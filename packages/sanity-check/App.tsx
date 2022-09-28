033a364cfd5b103c90e58ddb2de33d283d81bab2
Piotr Pliszczyński <piotr.pilczynski@frontegg.com>
2022-09-28 12:35:14 +0200

    Add sanity check to push workflow

---
+++ packages/sanity-check/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AdminPortal } from '@frontegg/react';
+
+function App() {
+    const openAdminBox = () => AdminPortal.show()
+    return (
+            <div>
+                <h1>Frontegg - React</h1>
+                <button onClick={openAdminBox}>Open admin box</button>
+            </div>
+
+    );
+}
+
+export default App;
