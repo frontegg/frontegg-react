5419ced05fb54bebc2faaca8fd0b58703da19e23
David Antoon <david@frontegg.com>
2022-10-12 11:32:05 +0300

    test changelog

--- .github/scripts/generate-changelog.js
+++ .github/scripts/generate-changelog.js
@@ -1,4 +1,3 @@
-
 export default async ({context, github, core}) => {
   const {default: fs} = await import('fs');
   let changelog = fs.readFileSync('./CHANGELOG.md', {encoding: 'utf8'});
@@ -11,7 +10,20 @@ export default async ({context, github, core}) => {
     sort: 'merged_at',
     direction: 'desc'
   });
-  console.log(pullsData);
+
+  let changelogStr = ''
+  const reactChanges = pullsData.filter(pull => pull.head.ref !== 'upgrade-admin-portal')
+  const adminPortalChanges = pullsData.find(pull => pull.head.ref === 'upgrade-admin-portal')
+
+  reactChanges.forEach(pull => {
+    changelogStr += ` - ${pull.title}\n`
+  });
+  changelogStr += '\n';
+
+  if (adminPortalChanges != null) {
+    changelogStr += adminPortalChanges.body;
+  }
+  console.log(changelogStr);
   // // changelog = changelog.substring(changelog.indexOf('# ['));
   // // changelog = changelog.substring(changelog.indexOf('\n')).trim();
   // changelog.substring(0, changelog.indexOf('# [') - 2);
