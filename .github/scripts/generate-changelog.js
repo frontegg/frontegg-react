2f058e41cd3b6312de0db808fd428196f31fbf34
David Antoon <david@frontegg.com>
2022-10-13 09:38:32 +0300

    test changelog 10

--- .github/scripts/generate-changelog.js
+++ .github/scripts/generate-changelog.js
@@ -1,8 +1,8 @@
 export default async ({context, github}) => {
   const {default: fs} = await import('fs');
-  const {default: path} = await import('path');
   let changelog = fs.readFileSync('./CHANGELOG.md', {encoding: 'utf8'});
-  const version = await import(path.join(__dirname,`../../lerna.json`)).version;
+  const {version} = JSON.parse(fs.readFileSync('./lerna.json', {encoding: "utf-8"}));
+
 
   const {data: pullsData} = await github.rest.pulls.list({
     owner: context.repo.owner,
@@ -23,24 +23,25 @@ export default async ({context, github}) => {
   const reactChanges = pullsFromLastRelease.filter(pull => pull.head.ref !== 'upgrade-admin-portal')
   const adminPortalChanges = pullsFromLastRelease.filter(pull => pull.head.ref === 'upgrade-admin-portal')
 
-  if (reactChanges.length > 0) {
-    changelogStr += "### @frontegg/react:\n"
-  }
-  reactChanges.forEach(pull => {
-    changelogStr += `- ${pull.title}\n`
-  });
-  changelogStr += '\n';
 
   adminPortalChanges.forEach(pull => {
     changelogStr += `${pull.body}\n`
+  });
+  changelogStr += '\n';
+  if (reactChanges.length > 0) {
+    changelogStr += `### React Wrapper ${version}:\n`
+  }
+  reactChanges.forEach(pull => {
+    changelogStr += `- ${pull.title}\n`
   });
 
   changelog = changelog.replace(/# Change Log\n/g, '');
   const dateNow = new Date();
   const date = `${dateNow.getFullYear()}-${dateNow.getMonth() + 1}-${dateNow.getDate()}`
-  let newChangelog = `# Change Log\n\n## [${version}](https://github.com/frontegg/frontegg-react/compare/v${lastRelease.title}...v${version}) (${date})\n\n`
+  let newChangelog = `# Change Log\n\n## [${version}](https://github.com/frontegg/frontegg-react/compare/${lastRelease.title}...v${version}) (${date})\n\n`
   newChangelog += changelogStr
   newChangelog += changelog.replace(/# Change Log\n/g, '\n');
-  console.log(newChangelog);
+
+  fs.writeFileSync('./CHANGELOG.md', newChangelog, {encoding: 'utf8'});
   return changelogStr;
 }
---
+++ .github/scripts/index.js
@@ -0,0 +1,1 @@
+export {default as generateChangeLog} from './generate-changelog.js'
