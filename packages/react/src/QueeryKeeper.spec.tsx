1be9a2febe6879515d6313d8955b5eb24f21bf48
Yuval Lotem <yuval.lotem@frontegg.com>
2023-11-12 18:48:53 +0200

    FR-14174-add-jest-testing-to-frontegg-react-sdk

---
+++ packages/react/src/QueeryKeeper.spec.tsx
@@ -0,0 +1,26 @@
+import { FronteggProvider } from './FronteggProvider';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import * as QueryKeeper from './queryKeeper';
+import 'whatwg-fetch';
+
+describe('Query keeper', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('Should call useQueryKeeper when no external history is provided ', async () => {
+    const mockUseQueryKeeper = jest.spyOn(QueryKeeper, 'useQueryKeeper');
+    render(<FronteggProvider contextOptions={{ baseUrl: 'mock-base-url' }} />, { wrapper: BrowserRouter as any });
+    expect(mockUseQueryKeeper).toHaveBeenCalled();
+  });
+
+  test('Should not call useQueryKeeper when external history is provided ', async () => {
+    const mockUseQueryKeeper = jest.spyOn(QueryKeeper, 'useQueryKeeper');
+    render(<FronteggProvider contextOptions={{ baseUrl: 'mock-base-url' }} history={true as any} />, {
+      wrapper: BrowserRouter as any,
+    });
+    expect(mockUseQueryKeeper).not.toHaveBeenCalled();
+  });
+});
