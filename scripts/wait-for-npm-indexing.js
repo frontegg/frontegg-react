c5c6b7420e2a75a49be093ad1d158a69bf1560bb
Rotem <rotem@frontegg.com>
2023-11-26 16:16:57 +0200

    FR-14304 - Adding logs to understand the timeout

--- scripts/wait-for-npm-indexing.js
+++ scripts/wait-for-npm-indexing.js
@@ -6,6 +6,8 @@ const delay = () => new Promise(resolve => setTimeout(() => resolve(), 1000));
 module.exports = async (github, packages, version) => {
   let timeout = 60;
 
+  console.log(`waiting for version ${version} of ${packages[0]}`)
+
   const checkPackages = async () => {
     if (timeout === 0) {
       throw Error('TIMEOUT: Some packages does not appear in NPM registry');
@@ -16,6 +18,7 @@ module.exports = async (github, packages, version) => {
     for (let i in packages) {
       try {
         const result = JSON.parse(execSync(`npm show ${packages[i]} --json dist-tags`).toString('utf8').trim());
+        console.log("result: ", result)
         allPackagesIndexed = allPackagesIndexed && (result.alpha === version || result.latest === version);
       } catch (e) {
         console.error('Failed to check version for', packages[i], e);
