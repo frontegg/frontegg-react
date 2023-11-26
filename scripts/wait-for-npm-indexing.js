801cb0dca8bdcba43057dbc51743ec197499a0ce
Rotem <rotem@frontegg.com>
2023-11-26 16:17:33 +0200

    FR-14304 - Adding logs to understand the timeout

--- scripts/wait-for-npm-indexing.js
+++ scripts/wait-for-npm-indexing.js
@@ -6,7 +6,7 @@ const delay = () => new Promise(resolve => setTimeout(() => resolve(), 1000));
 module.exports = async (github, packages, version) => {
   let timeout = 60;
 
-  console.log(`waiting for version ${version} of ${packages[0]}`)
+  console.log(`Waiting for publish of version ${version} of ${packages[0]}`)
 
   const checkPackages = async () => {
     if (timeout === 0) {
@@ -18,7 +18,7 @@ module.exports = async (github, packages, version) => {
     for (let i in packages) {
       try {
         const result = JSON.parse(execSync(`npm show ${packages[i]} --json dist-tags`).toString('utf8').trim());
-        console.log("result: ", result)
+        console.log("Found versions: ", result)
         allPackagesIndexed = allPackagesIndexed && (result.alpha === version || result.latest === version);
       } catch (e) {
         console.error('Failed to check version for', packages[i], e);
