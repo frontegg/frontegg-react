a4e57e8625de2bfe06f2b93ec5c8ecb36df18a7e
doregg <dor.moshe@frontegg.com>
2023-12-17 15:08:17 +0200

    hoc implementation

---
+++ packages/react/src/SteppedUpContent.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode, useEffect, useRef } from 'react';
+
+import { useStepUp, useIsSteppedUp } from '@frontegg/react';
+
+export interface SteppedUpProps {
+  maxAge?: number;
+  render?: (isSteppedUp: boolean) => React.ReactNode | null;
+  children?: ReactNode;
+}
+
+/**
+ * Stepped up content component that shows the wrapped content only when the user is stepped up
+ * The component triggers the step up flow if the user is not stepped up
+ * @param maxAge maximum time in second that the login is valid
+ * @param render render function to be called when user is stepped up
+ * @param children to be shown when user is stepped up (only if render not provided)
+ */
+export const SteppedUpContent: React.FC<SteppedUpProps> = ({ maxAge, render, children }) => {
+  const isSteppedUp = useIsSteppedUp({ maxAge });
+  const stepUp = useStepUp();
+  const isStepUpCalled = useRef(false);
+
+  useEffect(() => {
+    if (isSteppedUp) {
+      isStepUpCalled.current = false;
+      return;
+    }
+
+    if (isStepUpCalled.current) return;
+
+    stepUp({ maxAge });
+    isStepUpCalled.current = true;
+  }, [isSteppedUp, maxAge, stepUp]);
+
+  if (typeof render === 'function') {
+    return <>{render(isSteppedUp)}</>;
+  }
+
+  return isSteppedUp ? <>{children}</> : null;
+};
